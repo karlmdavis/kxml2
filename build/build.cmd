b24024a9ec228c4449ce2532b426ae7639d63d5d
haustein <haustein>
2002-04-07 23:17:49 +0000

    ...

--- build/build.cmd
+++ build/build.cmd
@@ -8,20 +8,19 @@ javadoc  org.kxml2.io org.xmlpull.v1
 
 cd %KXML2%\src
 
-javac org/kxml2/io/KXmlParser.java
+javac -g:none -O org/kxml2/io/KXmlParser.java
 
-jar cfM %KXML2%\lib\kxml2-src.zip org/kxml2/io/*.java
+jar cfM %KXML2%\lib\kxml2-src.zip org/kxml2/io/*.java META-INF/services/org.xmlpull.v1.XmlPullParserFactory
 jar cfM %KXML2%\lib\kxml2-min.zip org/kxml2/io/*.class
-jar cfM %KXML2%\lib\kxml2.zip org/kxml2/io/*.class
+jar cfM %KXML2%\lib\kxml2.zip org/kxml2/io/*.class META-INF/services/org.xmlpull.v1.XmlPullParserFactory
 
 cd %XMLPULL%\src\java\api
 
-javac org/xmlpull/v1/*.java
+javac  -g:none -O org/xmlpull/v1/*.java
 
 jar ufM %KXML2%/lib/kxml2-src.zip org/xmlpull/v1/*.java
 jar ufM %KXML2%/lib/kxml2.zip org/xmlpull/v1/*.class
 jar ufM %KXML2%/lib/kxml2-min.zip org/xmlpull/v1/XmlPullParser.class org/xmlpull/v1/XmlPullParserException.class
-
 
 
 cd %KXML2%\build
