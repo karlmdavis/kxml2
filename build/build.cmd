df6f5c998d1b9e7b52893663b254a2379dc54c5a
haustein <haustein>
2002-09-14 20:24:43 +0000

    yesnogame sample added

--- build/build.cmd
+++ build/build.cmd
@@ -12,7 +12,7 @@ javac -classpath %XMLPULL%\classes -g:none -O -d %KXML2%/classes org/kxml2/io/*.
 cd %KXML2%\classes
 
 jar cfM %KXML2%\lib\kxml2-min.zip org/kxml2/io/KXmlParser.class
-jar cfM %KXML2%\lib\kxml2.zip org/kxml2/io/*.class META-INF/services/org.xmlpull.v1.XmlPullParserFactory
+jar cfM %KXML2%\lib\kxml2.zip org/kxml2/io/*.class org/kxml2/kdom/*.class META-INF/services/org.xmlpull.v1.XmlPullParserFactory 
 
 cd %XMLPULL%\classes
 
